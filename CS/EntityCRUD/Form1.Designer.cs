d9ddbf46bbd8edfc411c8c102886a99dde6646e8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-10-25 15:45:11 +0300

    initial commit

---
+++ CS/EntityCRUD/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace EntityCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.orderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.orderBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(524, 363);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(531, 85);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(98, 23);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Preview Changes";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(531, 128);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(98, 23);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "SaveChanges";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 363);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.BindingSource orderBindingSource;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+    }
+}
+
---
+++ CS/EntityCRUD/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Windows.Forms;
+using DevExpress.XtraGrid.Views.Grid;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityCRUD
+{
+    public partial class Form1 : Form
+    {
+        MyContext  _context = new MyContext();
+        public Form1()
+        {
+            InitializeComponent();
+            gridView1.DataController.AllowIEnumerableDetails = true;
+            FillData();
+            _context.Persons.Load();
+            gridControl1.DataSource = _context.Persons.Local.ToBindingList();
+        }
+
+        private static void FillData()
+        {
+            using (var _cnt = new MyContext())
+            {
+                _cnt.Configuration.AutoDetectChangesEnabled = false;
+                if (_cnt.Persons.Any())
+                {
+                    return;
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    Person newPerson = new Person { FirstName = "First Name" + i, LastName = "Last Name" + i };
+                    Adress adr = new Adress();
+                    adr.City = "City" + i;
+                    newPerson.Adresses.Add(adr);
+                    _cnt.Persons.Add(newPerson);
+                }
+                _cnt.SaveChanges();
+            }
+        }
+        private void previewChanges()
+        {
+            var changeSet = _context.ChangeTracker.Entries();
+            int deletedCount = 0;
+            int changedCount = 0;
+            int addedCount = 0;
+            if (changeSet != null)
+                foreach (var dbEntityEntry in changeSet)
+                {
+                        switch (dbEntityEntry.State)
+                        {
+                            case EntityState.Deleted:
+                                deletedCount++;
+                            // process
+                                break;
+                            case EntityState.Unchanged:
+                                break;
+                            case EntityState.Added:
+                                addedCount++;
+                                // process
+                                break;
+                            case EntityState.Modified:
+                                changedCount++;
+                                break;
+                        }
+
+                }
+            string outp = string.Format("{0} added; {1} deleted; {2} modified",addedCount,deletedCount,changedCount);
+            MessageBox.Show(outp);
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            previewChanges();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            _context.SaveChanges();
+        }
+    }
+    public class MyContext : DbContext
+    {
+        public MyContext()
+            : base(@"Data Source=(localdb)\v11.0;Initial Catalog=TestContext;Integrated Security=True;MultipleActiveResultSets=True;Application Name=TestOfMasterDetail3")
+
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Adress>().Property(a => a.PersonID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Entity<Adress>().HasRequired(a => a.Person).WithMany(p => p.Adresses).HasForeignKey(a => a.PersonID);
+        }
+    }
+    public class Person
+    {
+        public Person()
+        {
+            Adresses = new List<Adress>();
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public virtual ICollection<Adress> Adresses { get; set; }
+    }
+    public class Adress
+    {
+        public string City { get; set; }
+        public int ID { get; set; }
+        public int? PersonID { get; set; }
+        [Required]
+        public virtual Person Person { get; set; }
+        public Adress()
+        {
+
+        }
+    }
+}
